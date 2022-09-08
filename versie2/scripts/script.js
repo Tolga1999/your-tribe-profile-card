1851e2102d0cf8322321cb41a9085c2d4e153c30
Tolga1999 <tolgadoganer@outlook.com>
2022-09-08 15:51:35 +0200

    added JS and animations

--- versie2/scripts/script.js
+++ versie2/scripts/script.js
@@ -1,16 +1,37 @@
 function readMore(){
-    let dots = document.getElementById("dots")
-    let more = document.getElementById("more")
-    let readMoreButton = document.getElementById("readMoreButton")
+    let more1 = document.getElementById("more1")
+    let more2 = document.getElementById("more2")
+    let more3 = document.getElementById("more3")
+    let mainImage1 = document.getElementById("mainImage1")
+    let mainImage2 = document.getElementById("mainImage2")
+    let mainImage3 = document.getElementById("mainImage3")
+    let mainH1 = document.getElementById("mainH1")
+    let secondH1 = document.getElementById("secondH1")
+    let thirdH1 = document.getElementById("thirdH1")
 
     // === also compares data types instead of only the value
-    if(dots.style.display === "none"){
-        dots.style.display = "inline"
-        more.style.display = "none"
-    }else{
-        dots.style.display = "none"
+    if(more1.style.display !== "none"){
+        more1.style.display = "none"
+        mainImage1.style.display = "none"
+        more2.style.display = "inline"
+        mainImage2.style.display = "inline"
+        mainH1.style.display = "none"
+        secondH1.style.display = "inline"
+    }else if(more2.style.display === "inline"){
         // more shows as inline so it can continue on the same line
-        more.style.display = "inline"
+        more2.style.display = "none"
+        more3.style.display = "inline"
+        mainImage2.style.display = "none"
+        mainImage3.style.display = "inline"
+        secondH1.style.display = "none"
+        thirdH1.style.display = "inline"
+    }else{
+        more1.style.display = "inline"
+        more3.style.display = "none"
+        mainImage1.style.display = "inline"
+        mainImage3.style.display = "none"
+        mainH1.style.display = "inline"
+        thirdH1.style.display = "none"
     }
 
 }
